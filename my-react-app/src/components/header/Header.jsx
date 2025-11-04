0bc0ca76e7ada92acf0dc68c38908724704a785d
TheGustavoDiaso <165075774+GustavoDiaso@users.noreply.github.com>
2025-11-04 14:31:07 -0300

    melhorando o header e criando animacoes

--- my-react-app/src/components/header/Header.jsx
+++ my-react-app/src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import css from "./Header.module.css";
 import logoIMGPath from "../../assets/images/logo.png"
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const navBarLinks = [
     { text: "Home", href: "#" },
@@ -11,30 +11,52 @@ const navBarLinks = [
 ]
 
 function Header() {
-    let [activeLink, setActiveLink] = useState('Home')
+    const [activeLinkText, setActiveLinkText] = useState('Home');
+    const ulRef = useRef(null); // Reference to the <ul></ul> element that contains the navbar links  
+    const slideRef = useRef(null); // Reference to the <div></div> element that represents the slide 
+
+    // this function will be called every time the component is mounted or whenever activeLink suffers any changes
+    useEffect(
+        ()=>{
+            //verifying rather the ulRef and slideRef variables are referencing the <ul> and <div> DOM elements respectively 
+            if (!ulRef.current || !slideRef.current){return}
+            
+            const slide = ulRef.current;
+            const activeLinkElement = ulRef.current.querySelector('[data-status="active"]');
+            
+            // defining the size of the slide element based on the active link he's behind 
+            slide.style.width = activeLinkElement.offsetWidth + 20;
+            slide.style.height = activeLinkElement.offsetHeight + 10;
+
+            console.dir(activeLinkElement)
+        },
+        [activeLinkText]
+    );
 
     return (
         <header className={css.header}>
             <nav>
-                <ul className={css.menu_options}>
+                <div className={css.slide} ref={slideRef}></div>
+
+                <ul className={css.menu_options} ref={ulRef}>
                     {
                         navBarLinks.map((link) => {
                             return (
-                                <li key={link.text}>
+                                <li 
+                                    key={link.text} 
+                                    className={css.nav_link} 
+                                    data-status={link.text == activeLinkText ? 'active' : ''}
+                                >
                                     <a
                                         href={link.href}
-                                        className={link.text == activeLink ? css.nav_link_active : css.nav_link}
-                                        onClick={() => { setActiveLink(link.text) }}
+                                        onClick={() => { setActiveLinkText(link.text) }}
                                     >
                                         {link.text}
                                     </a>
                                 </li>
                             )
                         })
-
                     }
-
-
                 </ul>
             </nav>
             <a href="#">
