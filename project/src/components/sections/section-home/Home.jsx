08a55d151157bb988fd61bf71e8ba2b32f4a8eed
GustavoDiaso <kingclash1906@gmail.com>
2025-11-07 20:17:50 -0300

    Melhorei o código do efeito escritura na section home

--- project/src/components/sections/section-home/Home.jsx
+++ project/src/components/sections/section-home/Home.jsx
@@ -1,5 +1,5 @@
 import css from "./Home.module.css"
-import { useRef, useState, useEffect } from "react"
+import { useRef, useState, useEffect, use } from "react"
 import devIMGPath from '../../../assets/images/manager.png'
 import linkedinIMGPath from '../../../assets/images/linkedin.png'
 import whatsappIMGPath from '../../../assets/images/whatsapp.png'
@@ -8,79 +8,77 @@ import curriculumPath from '../../../assets/curriculum/EnglishCV.pdf'
 import junimoDancingGIF from '../../../assets/images/junimo-dancing.gif'
 
 const socialMedias = [
-    {name: 'LinkedIn', href: 'https://www.linkedin.com/in/gustavodiaso', imgPath: linkedinIMGPath},
-    {name: 'GitHub', href: 'https://github.com/GustavoDiaso', imgPath: githubIMGPath},
-    {name: 'WhatsApp', href: 'https://wa.me/5511989155761', imgPath: whatsappIMGPath},
+    { name: 'LinkedIn', href: 'https://www.linkedin.com/in/gustavodiaso', imgPath: linkedinIMGPath },
+    { name: 'GitHub', href: 'https://github.com/GustavoDiaso', imgPath: githubIMGPath },
+    { name: 'WhatsApp', href: 'https://wa.me/5511989155761', imgPath: whatsappIMGPath },
 ];
 
-function Home(){
+function Home() {
 
     const junimoGIFRef = useRef(null);
     const [isJunimoJumping, setJunimoJumpingState] = useState(false);
-    
-    function handleJunimoClick(){
+
+    function handleJunimoClick() {
         // just check if the reference to the junimo gif exists first
-        if (!junimoGIFRef.current){return}
+        if (!junimoGIFRef.current) { return }
 
         // If the junimo is already jumping, the animation will not be executed again
-        if (isJunimoJumping){return}
-            
+        if (isJunimoJumping) { return }
+
         setJunimoJumpingState(true)
 
         // A timeout that will set the jumping state to false again, returning the junimo to the original position
-        setTimeout(()=>{
+        setTimeout(() => {
             setJunimoJumpingState(false)
         }, 200)
-        
+
     }
 
+    // State variables used inside the useEffect function. They will be used to create the effect of my
+    // name being written letter by letter in the Home section whenever the Home component is mounted
 
-    const [iIndex, setIIndex] = useState(0);
-    const [jIndex, setJIndex] = useState(0);
+    const [indexes, setIndexes] = useState({ i: 0, j: 0 });
     const [displayedFirstName, setDisplayedFirstName] = useState('');
     const [displayedLastName, setDisplayedLastName] = useState('');
 
+    // This function will be called whenever the Home section is mounted into the DOM 
+    // It's purpose is to make the effect of my name being written in the screen when the page is loaded
     useEffect(
         () => {
             const firstName = "Gustavo Henrique";
             const lastName = "de Oliveira Dias";
 
-            const intervalCode = setInterval(()=>{
-                setIIndex((currIIndex) => {
-                    if (currIIndex < firstName.length) {
-                
-                        setDisplayedFirstName((currDisplayedFirstName) => {
-                            return currDisplayedFirstName + firstName.charAt(currIIndex);
-                        });
-                        
-                        return currIIndex + 1; 
+            const intervalCode = setInterval(
+                () => {
+                    setIndexes(
+                        (currIndexesValues) => {
+                            const { i, j } = currIndexesValues;
 
-                    } 
-                    
-                    setJIndex((currJIndex)=>{
-                        if (currJIndex < lastName.length){
+                            if (i < firstName.length) {
+                                setDisplayedFirstName(
+                                    currDisplayedFirstName => currDisplayedFirstName + firstName.charAt(i)
+                                );
+                                return { i: i + 1, j: 0 };
+                            }
 
-                            setDisplayedLastName((currDisplayedLastName)=>{
-                                return currDisplayedLastName + lastName.charAt(currJIndex) 
-                            });
+                            if (j < lastName.length) {
+                                setDisplayedLastName(
+                                    currDisplayedLastName => currDisplayedLastName + lastName.charAt(j)
+                                );
+                                return { i: i, j: j + 1 };
+                            }
 
-                            return currJIndex + 1;
+                            clearInterval(intervalCode);
+                            return currIndexesValues;
+
                         }
-            
-                        clearInterval(intervalCode)
-
-                    });
-                });
-                    console.log('oi')
-                }, 
-                120
+                    )
+                }, 130 // time range between letter insertions
             );
-
-        }
-        , []
+        }, []
     )
 
-    return(
+    return (
         <section className={css.section_home}>
             <div className={css.dev_info}>
                 <h3 className={css.greeting}>Greetings! 🖖</h3>
@@ -91,11 +89,11 @@ function Home(){
 
                 <ul className={css.social_media_menu}>
                     {
-                        socialMedias.map((social_media)=>{
-                            return(
+                        socialMedias.map((social_media) => {
+                            return (
                                 <li key={social_media.name}>
                                     <a href={social_media.href} target="_blank">
-                                        <img src={social_media.imgPath} alt={social_media.name} className={css.social_media_image}/>
+                                        <img src={social_media.imgPath} alt={social_media.name} className={css.social_media_image} />
                                     </a>
                                 </li>
                             )
@@ -105,12 +103,12 @@ function Home(){
 
             </div>
 
-            <img src={devIMGPath} alt="dev-image.png" className={css.dev_img}/>
-            
+            <img src={devIMGPath} alt="dev-image.png" className={css.dev_img} />
+
             <div className={css.download_menu}>
-                <img 
-                    src={junimoDancingGIF} 
-                    alt="junimo-dancing.gif" 
+                <img
+                    src={junimoDancingGIF}
+                    alt="junimo-dancing.gif"
                     ref={junimoGIFRef}
                     onClick={handleJunimoClick}
                     className={
@@ -123,7 +121,7 @@ function Home(){
                 </a>
             </div>
 
-        </section>   
+        </section>
     )
 }
 export default Home
