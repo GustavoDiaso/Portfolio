4ad9202b0e3a69138c43a63b68e4df97d51e1b5d
TheGustavoDiaso <165075774+GustavoDiaso@users.noreply.github.com>
2025-11-12 11:59:16 -0300

    criando a secao skills

--- project/src/components/sections/Sections.jsx
+++ project/src/components/sections/Sections.jsx
@@ -1,12 +1,14 @@
 import css from "./Sections.module.css"
 import Home from './section-home/Home'
 import AboutMe from "./section-about-me/AboutMe"
+import Skills from "./section-skills/Skills"
 
 function Sections(){
     return(
         <section className={css.sections}>
             <Home/>
             <AboutMe/>
+            <Skills/>
         </section>
     )
 }
--- project/src/components/sections/section-skills/Skills.jsx
+++ project/src/components/sections/section-skills/Skills.jsx
@@ -1,9 +1,40 @@
 import css from "./Skills.module.css"
+import pythonIMGPath from "../../../assets/images/python.png"
+import flaskIMGPath from "../../../assets/images/flask.png"
+import reactIMGPath from "../../../assets/images/react.png"
+import javaScriptIMGPath from "../../../assets/images/js.png"
+import htmlIMGPath from "../../../assets/images/html.png"
+import cssIMGPath from "../../../assets/images/css.png"
+import gitHubIMGPath from "../../../assets/images/github.png"
+import mySqlIMGPath from "../../../assets/images/mysql.png"
+
+const skills = [
+    {name: 'Python', imgSource: pythonIMGPath},
+    {name: 'Flask API', imgSource: flaskIMGPath},
+    {name: 'React', imgSource: reactIMGPath},
+    {name: 'JavaScript', imgSource: javaScriptIMGPath},
+    {name: 'HTML 5', imgSource: htmlIMGPath},
+    {name: 'CSS 3', imgSource: cssIMGPath},
+    {name: 'GitHub', imgSource: gitHubIMGPath},
+    {name: 'MySQL', imgSource: mySqlIMGPath},
+];
 
 function Skills(){
     return (
         <section className={css.section_skills}>
-
+            <h1 className={css.title}>Skills and Technologies</h1>
+            <ul className={css.skills}>
+                {
+                    skills.map((skill)=>{
+                        return(
+                            <li className={css.skill} key={skill.name}>
+                                <img src={skill.imgSource} alt="" className={css.skill_img}/>
+                                {skill.name}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </section>
     )
 }
